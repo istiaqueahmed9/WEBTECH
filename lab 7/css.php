2dd84d5ba677cfd1c2438cb37cc76f145ff753b5
Istiaque Ahmed <72728927+istiaqueahmed9@users.noreply.github.com>
2021-08-07 16:09:10 +0600

    Add files via upload

---
+++ lab 7/css.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<style>
+		    
+			#p1{
+				background-color:rgb(0,78,162);
+				color:white;
+				text-align:justify;
+				font-family:consolas;
+			}
+			.red-white{
+				background-color:rgb(255,0,0);
+				color:white;
+				text-align:justify;
+				font-family:cambria;
+			}
+			#head1{
+				background-color:red;
+				
+			}
+			#sp1{
+				
+			}
+			#a1{
+				text-decoration:none;
+				text-align:center;
+				color:white;
+				background-color:rgb(73,127,175);
+				border:3px solid red;
+				padding:5px;
+				margin:5px;
+				border-radius:105px;
+				
+				
+			}
+			
+		</style>
+	</head>
+	<body>
+              <p id="p1">American International University - Bangladesh (AIUB) 
+              is a government approved private university founded in 1994 by Dr. Anwarul Abedin. 
+            The university is an independent organization with its own Board of Trustees. </p>
+            <h1 id="head1">This is h1</h1>
+		<span id="sp1">This is span</span><a id="a1" href="https://www.aiub.edu">Aiub</a>
+            	</body>
+</html>
